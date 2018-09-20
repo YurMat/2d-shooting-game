619b8fd9455b64a10bb5e97c7f9e5ace585b92f1
YurMat <yurimatsu28@gmail.com>
2018-09-20 16:54:05 +0900

    create Enemy Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    //spaceship コンポーネント
+    Spaceship spaceship;
+
+	// Use this for initialization
+	void Start () {
+
+        //Spaceshipコンポーネントを取得
+        spaceship = GetComponent<Spaceship>();
+
+        //ローカル座標のY軸のマイナス方向に移動
+        spaceship.Move(transform.up * -1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
