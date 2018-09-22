343c2c8e10022ce085972cb19a13e9e028e8dfe9
YurMat <yurimatsu28@gmail.com>
2018-09-22 10:53:33 +0900

    create Background Scroll

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    //スクロールするスピード
+    public float speed = 0.1f;
+
+    void Update()
+    {
+
+        float y = Mathf.Repeat(Time.time * speed, 1);
+
+        Vector2 offset = new Vector2(0, y);
+
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
