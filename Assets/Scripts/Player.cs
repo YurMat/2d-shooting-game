ad602b8208354a09ff6e37e3c9e362e912d6f344
YurMat <yurimatsu28@gmail.com>
2018-09-22 19:26:31 +0900

    change the way player controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,8 +33,29 @@ public class Player : MonoBehaviour
         // 移動する向きを求める
         Vector2 direction = new Vector2(x, y).normalized;
 
-        // 移動
-        spaceship.Move(direction);
+        //移動の制御
+        Move(direction);
+    }
+
+    void Move(Vector2 direction)
+    {
+        //画面左下のワールド座標をビューポートから取得
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        //画面右上のワールド座標をビューポートから取得
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        Vector2 pos = transform.position;
+
+        //移動量を加える
+        pos += direction * spaceship.speed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+
+        transform.position = pos;
+    
     }
 
     //ぶつかった瞬間に呼び出される
